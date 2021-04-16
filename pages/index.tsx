083f012aab1726a8a75866969950da425793e126
yushengtuimm <yushengtuimm@gmail.com>
2021-04-16 16:27:36 -0400

    initialize project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Whatsapp 2.0</title>
+      </Head>
+    </div>
+  );
+}
